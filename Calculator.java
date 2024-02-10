44d202a13276bebfc620ba789d62e2b9bd754379
Vaibhav-Rawat03 <vaibhavrawat2003@gmail.com>
2024-02-10 19:59:42 +0530

    Comitted by vaibhav

--- Calculator.java
+++ Calculator.java
@@ -14,7 +14,7 @@ class Calculator {
     public int subtract(int a, int b) {
         return a - b;
     }
-    public int multiply(int a, int b) {
-        return a * b;
+    public int multiply(int c, int d) {
+        return c * d;
     }
 }
