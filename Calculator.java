6b920ff054a2a792bb5aaa0c12a726a1db463c62
Shambhavee Srivastava <110680194+shambhaveesrivastava12@users.noreply.github.com>
2024-02-10 19:35:28 +0530

    UPDATE: added java file

---
+++ Calculator.java
@@ -0,0 +1,16 @@
+class Calculator {
+    
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        System.out.println("Addition: " + calculator.add(10, 5));
+        System.out.println("Subtraction: " + calculator.subtract(10, 5));
+    }
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+}
